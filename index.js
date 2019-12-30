6b76201d1daa48b38e10c8ebc6cbb2a2f710f9eb
Jacob Roush <jacobroushmail@gmail.com>
2019-12-29 20:19:56 -0600

    added cors header and add new doc when not found

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const firestore = new Firestore({
 });
 
 exports.addLike = (request, response) => {
-    const articleRef = firestore.doc('articles/test');
+    response.set('Access-Control-Allow-Origin', "*");
+    const articleRef = firestore.doc('articles/a article with spaces');
     articleRef.get()
         .then(doc => {
             if (!doc.exists) {
-                console.log('Creating new article');
+                articleRef.add({ Likes: 1 }).then(documentReference => {
+                    console.log(`Added document with name: ${documentReference.id}`);
+                });
                 response.status(500).send('doesn\'t exist');
             } else {
                 articleRef.update('Likes', Firestore.FieldValue.increment(1));
@@ -21,11 +24,10 @@ exports.addLike = (request, response) => {
             console.log('Error getting document', err);
             response.status(500).send('Error');
         });
-
-    // response.status(200).send('liked');
 };
 
 exports.getLikes = (request, response) => {
+    response.set('Access-Control-Allow-Origin', "*");
     console.log(request);
     const articleRef = firestore.doc('articles/test');
     articleRef.get()
